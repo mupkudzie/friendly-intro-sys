45f21c82df0b40d01057d64ad9b9ef5cf4454c33
mupkudzie <mupondikudzaim@gmail.com>
2026-03-18 07:55:25 +0200

    Remove OpenCV dependencies and cv_service

--- cv_service/__init__.py
+++
@@ -1,1 +0,0 @@
-# CV Service package initialization
--- cv_service/run.py
+++
@@ -1,6 +0,0 @@
-# Entry point for the application
-from app import create_app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000)
